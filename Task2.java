06ce73e3612f66814db8c886e3f60bd4c3c945e6
Vikassahu62 <vikassahu54927@gamil.com>
2025-01-08 12:31:08 +0530

    second task added

---
+++ Task2.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+public class Task2 
+{ 
+  public static void main(String[]args) 
+  { Scanner s=new Scanner(System.in);
+  
+    System.out.println("Enter first number");
+    int a=s.nextInt(); 
+	
+	System.out.println("Enter second number");
+	int b=s.nextInt(); 
+	
+	int sum=a+b;
+	System.out.println("The sum of two numbers: "+ sum); 
+	}
+} 
